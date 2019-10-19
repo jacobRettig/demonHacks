8a4bede43dedda19dfd9a739ed55d1792686b5a4
jacobRettig <jacobRettig@users.noreply.github.com>
2019-10-19 13:03:43 -0500

    Empty base template

---
+++ base.py
@@ -0,0 +1,12 @@
+'''Does the website stuff'''
+def launchWebsite():
+  raise NotImplementedError()
+
+'''Template to generate section HTML
+  Graph will be a matplotlib object, this function will handle loading the graph'''
+def genSubSection(subtitle, question, auxInfo, graph=None):
+  raise NotImplementedError()
+
+
+if __name__ == '__main__':
+  launchWebsite()
