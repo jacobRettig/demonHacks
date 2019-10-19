8469805d3d7f6641fa4d3d91e8a60b05d33c641c
jacobRettig <jacobRettig@users.noreply.github.com>
2019-10-19 13:49:40 -0500

    Unstyled version of website, graphs have also not yet been implemented

--- base.py
+++ base.py
@@ -1,12 +1,48 @@
+from flask import Flask
+from functools import reduce
+
+app = Flask(__name__)
+
+'''First example subsection'''
+def getTestQuestion():
+  return genSubSection('Subtitle', 'Question', 'Auxiliary Info')
+
+'''Second example subsection'''
+def getSecondTestQuestion():
+  return genSubSection('The important topic', 'What is the meaning of life?', 'Life is meaningless')
+
+'''List of all functions that generate content'''
+questions = [getTestQuestion, getSecondTestQuestion]
+
 '''Does the website stuff'''
-def launchWebsite():
-  raise NotImplementedError()
+@app.route('/')
+def mainPage():
+  content = '''<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Chicago Transit Impacts</title>
+      <meta charset="UTF-8">
+      <style>
+      </style>
+    </head>
+    <body>
+      <h1>Chicago Transit Impacts</h1>{}
+    </body>
+  </html>'''.format(reduce(lambda a,b:'{}\n{}'.format(a, b()), questions, ''))
+  return content
+
 
 '''Template to generate section HTML
   Graph will be a matplotlib object, this function will handle loading the graph'''
 def genSubSection(subtitle, question, auxInfo, graph=None):
-  raise NotImplementedError()
+  content = '''<hr/>
+  <h2>{}</h2>
+  <p>{}</p>
+  <img alt="Images not yet implemented" />
+  <p>{}</p>'''.format(subtitle, question, auxInfo)
+  return content
 
 
 if __name__ == '__main__':
-  launchWebsite()
+  app.run()
+  print('done')
