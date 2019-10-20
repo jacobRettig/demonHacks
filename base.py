5bdac20798e74e19e36cb6df1bf1039813fa0986
jacobRettig <jacobRettig@users.noreply.github.com>
2019-10-20 09:17:56 -0500

    Moved car usage to main file thus fixing chart bug

--- base.py
+++ base.py
@@ -1,13 +1,43 @@
 from flask import Flask, Response, url_for
 from functools import reduce
 import numpy as np
+import pandas as pd
+
+from datetime import date
 import io
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+
+
 app = Flask(__name__)
+
+
+def getQuestionData():
+  data = pd.read_csv('Average_Daily_Traffic_Counts.csv')
+  data['Date'] = data.apply(lambda x:date(int(x['Date of Count'].split('/')[2]), int(x['Date of Count'].split('/')[0]), int(x['Date of Count'].split('/')[1])), axis=1)
+  data = data[data['Date'] < date(2017, 1, 1)]
+  earliestDay = min(data['Date'])
+  data['nthSecond'] = data.apply(lambda x:(x['Date'] - earliestDay).total_seconds(), axis=1)
+  data['Month'] = data.apply(lambda x:x['Date'].month, axis=1)
+  return data
+
+def getQuestionGraph(data):
+  fig, axs = plt.subplots()
+  axs.hist(x=data['Month'], weights=data['Total Passing Vehicle Volume'], bins=12)
+  #plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'])
+  axs.get_xaxis().set_ticks(range(1,14))
+  axs.get_xaxis().set_ticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', '.'])
+  axs.get_yaxis().set_ticklabels([])
+  axs.set_title('Amount of Vehicles on Street by Month')
+  return fig
+
+def getCarQuestion():
+  data = getQuestionData()
+  fig = getQuestionGraph(data)
+  return genSubSection('Vehicle Usage', 'What are the most and least popular months for vehicle usage in Chicago?', '', fig)
 
 '''First example subsection'''
 def getTestQuestion():
@@ -32,13 +62,14 @@ def getSecondTestQuestion():
 
 '''Takes a matplotlib Figure and returns raw data that web server gives to user'''
 def figToResponse(fig):
+  print('Figure is {}'.format(fig))
   output = io.BytesIO()
   FigureCanvasAgg(fig).print_png(output)
   return Response(output.getvalue(), mimetype='image/png')
 
 
 '''List of all functions that generate content'''
-questions = [getTestQuestion, getSecondTestQuestion]
+questions = [getCarQuestion]
 questionNames = list()
 
 '''Global chart object'''
